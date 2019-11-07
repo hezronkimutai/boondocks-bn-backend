c1e0149712206d6447246fd788eb99ee1960dd36
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-11-07 10:47:49 +0200

    chore(error handling): Central errorHandler - add errorHandler helper file - added not found error handler - added logger on the error handler - added `/err` to handle the error [finishes #169590224]

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import { urlencoded, json } from 'body-parser';
 import cors from 'cors';
 import errorhandler from 'errorhandler';
 import router from './routes/index';
+import ErrorHandler from './utils/error';
+import logger from './utils/winston';
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isDevelopment = process.env.NODE_ENV;
 
 const app = express();
 
@@ -15,48 +17,44 @@ app.use(cors());
 app.use(urlencoded({ extended: false }));
 app.use(json());
 
-
-if (!isProduction) {
-  app.use(errorhandler());
-}
-
 // used in testing heroku
 app.get('/', (req, res) => res.status(200).json({
-  status: 200,
   message: 'Welcome to Barefoot Nomad'
 }));
 
-app.use(router);
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// to throw an error to the logger
+app.get('/err', (req, res) => {
+  throw new ErrorHandler('Errror here');
 });
 
+app.use(router);
+app.use((req, res, next) => res.status(404).json({
+  error: 'Not found'
+}));
 
-if (!isProduction) {
-  app.use((err, req, res, next) => {
-    console.log(err.stack);
-
-    res.status(err.status || 500);
-
-    res.json({
-      errors: {
-        message: err.message,
-        error: err
-      }
-    });
-  });
-}
-
-
+// development error handler middleware
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.json({
-    errors: {
-      message: err.message,
-      error: {}
-    }
+  if (isDevelopment !== 'development') {
+    return next(err);
+  }
+  logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${
+    req.ip
+  } - Stack: ${err.stack}`);
+
+  return res.status(err.statusCode).json({
+    message: err.message,
+    info: 'check logs for more info'
+  });
+});
+
+// Production and testing error handler middleware
+app.use((err, req, res, next) => {
+  logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${
+    req.ip
+  } - Stack: ${err.stack}`);
+
+  return res.status(err.statusCode).json({
+    message: err.message
   });
 });
 
