79cc2833577ae1342ce3f21787dbcceb55c23128
Mastel22 <pmastel82@gmail.com>
2019-11-11 16:02:49 +0200

    feature(validation):Signin and Signup validations
    - added the signup schema
    - added the signin schema

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -3,15 +3,13 @@ import Joi from '@hapi/joi';
 const signupSchema = {
   firstName: Joi.string().strict().trim().required(),
   lastName: Joi.string().strict().trim().required(),
-  email: Joi.string().strict().trim().email()
-    .required(),
+  email: Joi.strict().trim().email().required(),
   password: Joi.string().alphanum().min(8).required()
     .strict(),
 };
 
 const signinSchema = {
-  email: Joi.string().strict().trim().email()
-    .required(),
+  email: Joi.strict().trim().email().required(),
   password: Joi.string().alphanum().min(8).required(),
 };
 
