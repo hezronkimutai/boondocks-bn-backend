28aaf7ea4de578e6f6eae868251fc17c8dbdd15b
shaphan <chaphannzabonimana@gmail.com>
2019-11-12 16:44:49 +0200

    feature(signup): register and generate token - Create user model and migrations - Create seeds - Validate request parameters - Hash password and store user - Generate JWT token and return response
    [Finishes #169257394]

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -3,13 +3,15 @@ import Joi from '@hapi/joi';
 const signupSchema = {
   firstName: Joi.string().strict().trim().required(),
   lastName: Joi.string().strict().trim().required(),
-  email: Joi.strict().trim().email().required(),
+  email: Joi.string().strict().trim().email()
+    .required(),
   password: Joi.string().alphanum().min(8).required()
     .strict(),
 };
 
 const signinSchema = {
-  email: Joi.strict().trim().email().required(),
+  email: Joi.string().strict().trim().email()
+    .required(),
   password: Joi.string().alphanum().min(8).required(),
 };
 
