cba88de1cc591c2bff86d540c1ef98d589e85360
RaymondMwaura <raywachaga@gmail.com>
2019-11-14 13:28:35 +0200

    feat(login): implement cors on endpoints
    	-implement cors on all API endpoints
    
    [Delivers #169642565]

--- src/app.js
+++ src/app.js
@@ -10,7 +10,25 @@ const isDevelopment = config.env;
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  // We shall remove this URL for production
+  'http://localhost',
+  // Our front end URL to be added here in phase 2 of SIMS
+];
+app.use(cors({
+  origin(origin, callback) {
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not '
+                + 'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    } return callback(null, true);
+  },
+  // To enable HTTP cookies over CORS
+  credentials: true,
+}));
 
 app.use(urlencoded({ extended: false }));
 app.use(json());
